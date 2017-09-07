16f62e2d110d8e7bc7d6e8ad8f855d3e94cc8aaf
Shanto16 <shakil.glostars@gmail.com>
2017-09-08 03:17:56 +0600

    new update

--- app/src/main/java/mcgyvers/mobitrip/Current_trip.java
+++ app/src/main/java/mcgyvers/mobitrip/Current_trip.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +20,7 @@ import android.widget.Toast;
 
 public class Current_trip extends Fragment {
 
-    Button team_expense;
+    Button team_expense,my_expense;
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
@@ -30,6 +31,59 @@ public class Current_trip extends Fragment {
 
 
         team_expense = rootView.findViewById(R.id.team_expense_button);
+        my_expense = rootView.findViewById(R.id.my_expense_button);
+
+
+        my_expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // custom dialog
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.my_expense_dialog);
+                dialog.setTitle("My Expense");
+
+//                // set the custom dialog components - text, image and button
+//                TextView name_text = (TextView) dialog.findViewById(R.id.expense_name_text);
+//                name_text.setText("Edson");
+//
+//                EditText expense_edit =(EditText) dialog.findViewById(R.id.expense_expense_edit);
+//                expense_edit.setText("2540");
+//                ImageView call_member = (ImageView) dialog.findViewById(R.id.expense_call_member);
+//
+//                call_member.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        Toast.makeText(getActivity(), "It will open up the dialer of phone", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+                Button save_button = dialog.findViewById(R.id.my_expense_save);
+
+                save_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                ImageView addExpense = dialog.findViewById(R.id.add_new_expense);
+                addExpense.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getActivity(), "A new 'my_expense_model.xml' will be added in this listView here. ", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+
+
+                dialog.show();
+
+            }
+        });
+
+
+
 
 
         team_expense.setOnClickListener(new View.OnClickListener() {
@@ -37,29 +91,28 @@ public class Current_trip extends Fragment {
             public void onClick(View view) {
                 // custom dialog
                 final Dialog dialog = new Dialog(context);
-                dialog.setContentView(R.layout.expense_model);
+                dialog.setContentView(R.layout.model_team_expense);
                 dialog.setTitle("Team Expense");
 
-                // set the custom dialog components - text, image and button
-                TextView name_text = (TextView) dialog.findViewById(R.id.expense_name_text);
-                name_text.setText("Edson");
+                ListView team_expense = dialog.findViewById(R.id.team_expense_listview);
+                Button ok_button = dialog.findViewById(R.id.team_expense_dismiss);
 
-                EditText expense_edit =(EditText) dialog.findViewById(R.id.expense_expense_edit);
-                expense_edit.setText("2540");
-                ImageView call_member = (ImageView) dialog.findViewById(R.id.expense_call_member);
-
-                call_member.setOnClickListener(new View.OnClickListener() {
+                ok_button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Toast.makeText(getActivity(), "It will open up the dialer of phone", Toast.LENGTH_SHORT).show();
+
+                        dialog.dismiss();
                     }
                 });
+
+
 
 
                 dialog.show();
 
             }
         });
+
 
         return rootView;
     }
